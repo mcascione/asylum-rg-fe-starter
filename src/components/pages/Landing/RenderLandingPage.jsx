71d4a98fd4c8c66bc8f9110becd5a4acc05e4622
Miranda Cascione <miranda.cascione@gmail.com>
2023-06-06 13:56:32 -0700

    Graph Images and Captions Rendering

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -31,8 +31,35 @@ function RenderLandingPage(props) {
         </div>
       </div>
 
-      {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <img
+          src={GrantRatesByOfficeImg}
+          alt="Grant Rates By Office"
+          className="gr-office-img"
+        ></img>
+        <p className="grant-rates-by-office-graph-container">
+          Search Grant Rates By Office
+        </p>
+
+        <img
+          src={GrantRatesByNationalityImg}
+          alt="Grant Rates By Nationality"
+          className="gr-nationality-img"
+        ></img>
+        <p className="grant-rates-by-nationality-container">
+          Search Grant Rates By Nationality
+        </p>
+
+        <img
+          src={GrantRatesOverTimeImg}
+          alt="Grant Rates Over Time"
+          className="gr-overtime-img"
+        ></img>
+        <p className="grant-rates-over-time-container">
+          Search Grant Rates Over Time
+        </p>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
